047a336a5b29909bc718e57fc50b6502a120569c
luistung <dongliang1986@gmail.com>
2013-12-26 19:20:40 +0800

    Update mapred.py
    删除process_stream，用process统一处理。增加默认parser和handler。

--- mapred.py
+++ mapred.py
@@ -1,12 +1,54 @@
-﻿#!/dev/python
+#!/bin/env python
 import sys
 
+class Splitter:
+    def __init__(self, sep, keynum):
+        self.sep = sep
+        self.keynum = keynum
+        
+    @staticmethod
+    def _find_nth_sep(s, sep, n):
+        if n == 0: return 0
+        count = 0
+        pos = 0
+        while count < n and pos < len(s):
+            if s[pos] == sep: 
+                count += 1
+                if count == n: return pos
+            pos += 1
+        if pos == len(s) and count + 1 == n: return pos
+        raise ValueError('separator not found')
+           
+    def __call__(self, line):
+        line = line.strip(self.sep)
+        pos = Splitter._find_nth_sep(line, self.sep, self.keynum)
+        if pos == 0:
+            return "", line
+        if pos == len(line):
+            return line, ""
+        return line[:pos], line[pos + 1:]
+
+class Map_handler:
+    def __init__(self, sep):
+        self.sep = sep
+    
+    def __call__(self, key, value):
+        yield '%s%s%s' % (key, self.sep, value)
+        
+class Reduce_handler:
+    def __init__(self, sep):
+        self.sep = sep
+    
+    def __call__(self, key, value_it):
+        for value in value_it:
+            yield '%s%s%s' % (key, self.sep, value) 
+
 class Mapper:
-    def __init__(self, parser, handler):
+    def __init__(self, parser=Splitter('\t', 1), handler=Map_handler('\t')):
         self.handler = handler
         self.parser = parser
     
-    def process(self, fin, fout):
+    def process(self, fin=sys.stdin, fout=sys.stdout):
         for i in fin:
             for j in self.handler(*self.parser(i.rstrip())):
                 print >> fout, j
@@ -40,17 +82,16 @@ class ExtIter:
         
 
 class Reducer:
-    def __init__(self, parser, handler=None, handler_stream=None):
+    def __init__(self, parser=Splitter('\t', 1), handler=Reduce_handler('\t')):
         self.handler = handler
-        self.handler_stream = handler_stream
         self.last_key = None
         self.parser = parser
 
-    def parse_it(self, fin):
+    def _parse_it(self, fin):
         for i in fin:
             yield self.parser(i.rstrip())
 
-    def my_it(self, it):
+    def _my_it(self, it):
         while it.has_next():
             key, value = it.peak()
             assert key is not None
@@ -62,20 +103,12 @@ class Reducer:
                 self.last_key = key
                 yield value
 
-    def it_gene(self, it):
+    def _it_gene(self, it):
         while it.has_next():
             self.last_key = it.peak()[0]
-            yield self.my_it(it)
+            yield self._my_it(it)
 
     def process(self, fin=sys.stdin, fout=sys.stdout):
-        if self.handler is None:
-            raise TypeError("'handle' must be provided")
-        for it in self.it_gene(ExtIter(self.parse_it(fin))):
+        for it in self._it_gene(ExtIter(self._parse_it(fin))):
             for line in self.handler(self.last_key, it):
                 print >>fout, line
-
-    def process_stream(self, fin=sys.stdin, fout=sys.stdout):
-        if self.handler_stream is None:
-            raise TypeError("'handle_steam' must be provided")
-        for it in self.it_gene(ExtIter(self.parse_it(fin))):
-            self.handler_stream(self.last_key, it, fout)
